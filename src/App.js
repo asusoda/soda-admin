4d71ab8359e6e6bdb42327e477c0a16ec22af2d5
code-wolf-byte <hackerstark37@gmail.com>
2024-12-02 13:42:42 -0700

    Added new UserPage

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import TokenRetrival from './pages/TokenRetrival';
 import HomePage from './pages/HomePage';
 import UserPage from './pages/UserPage';
 import LeaderBoard from './pages/LeaderBoard';
+import CreateUserPage from './pages/CreateUserPage';
 
 function App() {
   
@@ -34,6 +35,7 @@ function App() {
         <Route path='/500' element = {<ServerError />} />
         <Route path='/users' element={<UserPage />} />
         <Route path='/leaderboard' element={<LeaderBoard />} />
+        <Route path='/createuser' element={<CreateUserPage />} />
       </Routes>
       <ToastContainer />
     </BrowserRouter>
